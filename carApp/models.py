b8e3580f395139ac048924c9b4c706f8788e4fa3
MehediMashfan086 <mehedihasan62086@gmail.com>
2021-12-03 23:42:03 +0600

    Saving to GitHub

--- carApp/models.py
+++ carApp/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+# Category
+class Category(models.Model):
+    title = models.CharField(max_length=100)
+    image = models.ImageField(upload_to = "category_imgs/")
+    
+    class Meta:
+        verbose_name_plural='Categories'
+    
+    def __str__(self):
+        return self.title
+    
+# Brand
+class Brand(models.Model):
+    title = models.CharField(max_length=100)
+    image = models.ImageField(upload_to = "brand_imgs/")
+    
+    def __str__(self):
+        return self.title
+    
+# Color
+class Color(models.Model):
+    title = models.CharField(max_length=100)
+    color_code = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.title
+
+# Size
+class Size(models.Model):
+    title = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.title
