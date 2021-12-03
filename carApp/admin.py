44b341177dab9794ef3af954a086c9a2b75c9174
MehediMashfan086 <mehedihasan62086@gmail.com>
2021-12-04 01:18:02 +0600

    Saving to GitHub

--- carApp/admin.py
+++ carApp/admin.py
@@ -6,3 +6,8 @@ admin.site.register(Category)
 admin.site.register(Brand)
 admin.site.register(Color)
 admin.site.register(Size)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display=('id','title','brand','color','size','price', 'status')
+    list_editable = ('status',)
+admin.site.register(Product, ProductAdmin)
--- carApp/models.py
+++ carApp/models.py
@@ -34,14 +34,19 @@ class Size(models.Model):
     def __str__(self):
         return self.title
 
-# Color
+# Product
 class Product(models.Model):
     title = models.CharField(max_length=100)
+    image = models.ImageField(upload_to = "product_imgs/")
     slug = models.CharField(max_length=500)
     detail = models.TextField()
     specification = models.TextField()
     price = models.PositiveBigIntegerField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+    color = models.ForeignKey(Color, on_delete=models.CASCADE)
+    size = models.ForeignKey(Size, on_delete=models.CASCADE)
+    status = models.BooleanField(default=True)
     
     def __str__(self):
         return self.title
